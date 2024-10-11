a99ad859dc4bfe04d21acdb650a76bf84a9589be
RaviprasadKolli <raviprasad.knr@gmail.com>
2024-10-12 01:05:10 +0530

    Reset button updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react';
 import './App.css';
 
 let App = () => {
-  const [count, setCount] = useState(0);
+  let [count, setCount] = useState(0);
   let handleClick = () => {
     if (count < 10) {
       setCount(count + 1);
     }
   };
-  const handleDecrement = () => {
+  let handleDecrement = () => {
     if (count > 0) {
       setCount(count - 1);
     }
   };
+  let reset = ()=>{
+    setCount(count=0)
+  }
   return (
     <>
       <div>
@@ -22,6 +25,7 @@ let App = () => {
         <h2>Count :- {count}</h2>
         <button onClick={handleClick}>Increment</button>
         <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
       </div>
     </>
   );
